de587d3beafa7e40c6403117bf62b20ca30a3f92
Ryan Carlos <181878315+ryanccarlos@users.noreply.github.com>
2026-05-09 23:07:52 -0300

    pranking a friend with a image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 function Home () {
     return (
         <div>
-            <h1>Gif aleatório que o Copilot me recomendou</h1>
-            <p>--------------------------------------------------------------------------------------------------------------</p>
-            <img src="https://media.giphy.com/media/3o7aD2saalBwwftBIY/giphy.gif" alt="gif" />
+            <h1 style={{ fontSize: "46px"}}>Eu sei a origem desse aviso viu</h1>
+            <img src="images/image.png" alt="Aviso Vrido Quebrado"/>
         </div>
     )
 }
