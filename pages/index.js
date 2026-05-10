9a9651503ea32c74a8b1ce2fdede317d7aa0cbfb
Ryan Carlos <181878315+ryanccarlos@users.noreply.github.com>
2026-05-09 22:37:05 -0300

    random gif copilot suggest

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home () {
-    return <h1>Te amo ❤️❤️</h1>
+    return (
+        <div>
+            <h1>Gif aleatório que o Copilot me recomendou</h1>
+            <p>--------------------------------------------------------------------------------------------------------------</p>
+            <img src="https://media.giphy.com/media/3o7aD2saalBwwftBIY/giphy.gif" alt="gif" />
+        </div>
+    )
 }
 
 export default Home;
